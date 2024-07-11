e417bbc9470bdfb07a3c1cd5b527fb2cdce2fd0e
iagovi <iagovieiraprofi@gmail.com>
2024-07-11 11:13:44 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,7 +1,11 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,22 +15,18 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
   private static final long serialVersionUID = 1L;
-  
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String email;
   private String phone;
   private String password;
-
-
-
-
-
-
-
-
 
 
 
